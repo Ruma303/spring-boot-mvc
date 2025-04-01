7d1e1c21526bdbce66e93abc834b7b6667457aae
Ruma <matteoh8@hotmail.it>
2025-04-01 16:51:15 +0200

    routes

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    @GetMapping("/users")
+    public String getUsers() {
+        return "Lista degli utenti";
+    }
+    
+    @PostMapping("/users")
+    public String createUser() {
+        return "Utente creato";
+    }
+
+    @PutMapping("/users/{id}")
+    public String updateUser(@PathVariable int id) {
+        return "Utente con ID " + id + " aggiornato";
+    }
+
+    @DeleteMapping("/users/{id}")
+    public String deleteUser(@PathVariable int id) {
+        return "Utente con ID " + id + " eliminato";
+    }
+    
+    @GetMapping("/user")
+    public String getUser(
+    		@RequestParam String name,
+    		@RequestParam("last_name") String lastName
+    		) {
+        return "Utente con nome " + name + " e cognome " + lastName;
+    }
+
+}
