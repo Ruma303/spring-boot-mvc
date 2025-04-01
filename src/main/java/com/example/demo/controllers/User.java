812408fd07c415f5d9e15dc93a059e9180479d7b
Ruma <matteoh8@hotmail.it>
2025-04-01 17:52:07 +0200

    User

---
+++ src/main/java/com/example/demo/controllers/User.java
@@ -0,0 +1,41 @@
+package com.example.demo.controllers;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+	
+	@JsonProperty("primoNome")
+	private String name;
+	
+	@JsonProperty("Cognome")
+	private String lastName;
+	
+	public User() {
+		super();
+	}
+	
+	public User(String name, String lastName) {
+		super();
+		this.name = name;
+		this.lastName = lastName;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [name=" + name + ", lastName=" + lastName + "]";
+	}
+
+}
