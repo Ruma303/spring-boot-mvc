ea8b4eb6502848897b04d4f4b577b10fa7e996b5
Ruma <matteoh8@hotmail.it>
2025-04-14 23:02:09 +0200

    .

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -12,35 +12,43 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping()
     public String getUsers() {
         return "Lista degli utenti";
     }
 
-    @PostMapping("/users")
-    public String createUser(@RequestBody User user) {
-        return "Utente creato: " + user.toString();
-    }
-
-    @PutMapping("/users/{id}")
-    public String updateUser(@PathVariable int id) {
-        return "Utente con ID " + id + " aggiornato";
-    }
-
-    @DeleteMapping("/users/{id}")
-    public String deleteUser(@PathVariable int id) {
-        return "Utente con ID " + id + " eliminato";
-    }
-
-    @GetMapping("/user")
+    @GetMapping("/{name}/{lastName}")
     public String getUser(
-            @RequestParam String name,
-            @RequestParam("last_name") String lastName
+            @PathVariable String name,
+            @PathVariable("lastName") String lastName
     ) {
         return "Utente con nome " + name + " e cognome " + lastName;
     }
 
+    @GetMapping("/search")
+    public String search(
+            @RequestParam(name = "name", required = true, defaultValue = "N/D") String name,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit,
+            @RequestParam(name = "sort", required = false, defaultValue = "asc") String sortOrder
+    ) {
+        return "Utenti trovati per la ricerca: " + name + ", Limite: " + limit + ", Ordinamento: " + sortOrder;
+    }
+
+    @PostMapping()
+    public String createUser(@RequestBody User user) {
+        return "Utente creato: " + user.toString();
+    }
+
+    @PutMapping("{id}")
+    public String updateUser(@PathVariable int id) {
+        return "Utente con ID " + id + " aggiornato";
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteUser(@PathVariable int id) {
+        return "Utente con ID " + id + " eliminato";
+    }
 }
