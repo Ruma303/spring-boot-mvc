4b04da21a676171ca07182bf2977beeef731e682
Ruma <matteoh8@hotmail.it>
2025-04-14 21:27:36 +0200

    cambio api

--- src/main/java/com/example/demo/controllers/FirstController.java
+++ src/main/java/com/example/demo/controllers/FirstController.java
@@ -4,7 +4,7 @@ import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class FirstController {
 
 	@GetMapping("/hello")
